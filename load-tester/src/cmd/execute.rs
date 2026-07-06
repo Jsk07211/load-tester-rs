720314b48237e875379ba74f7c417410793df9b6
Jaz Ku <jsku@dons.usfca.edu>
2026-07-05 23:11:03 -0700

    remove redundant Args declaration

--- load-tester/src/cmd/execute.rs
+++ load-tester/src/cmd/execute.rs
@@ -1,27 +1,6 @@
 use crate::config::Config;
 use crate::metrics::get_summary;
 use crate::worker::{self, SharedState};
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-pub struct Args {
-    /// Target URL to load test
-    #[arg(short, long, default_value = "http://localhost:8080/ping")]
-    pub endpoint: String,
-
-    /// Number of virtual users (concurrent in-flight requests, sustained for the full test duration)
-    #[arg(short, long, default_value_t = 5)]
-    pub virtual_users: u32,
-
-    /// Duration of test
-    #[arg(short, long, default_value_t = 5)]
-    pub duration_s: u64,
-
-    /// HTTP method
-    #[arg(short, long, default_value = "GET")]
-    pub method: String,
-}
 
 pub async fn run(config: Config) -> anyhow::Result<()> {
     let initial_state = SharedState::try_from(&config)?;
