be0b6b33d5249802133604d1d4905e594f9a2470
Jaz Ku <jazsku@gmail.com>
2026-07-22 23:12:29 -0700

    add post endpoint for testing

--- test-server/src/main.rs
+++ test-server/src/main.rs
@@ -2,18 +2,30 @@ use axum::{
     Json, Router,
     routing::{get, post},
 };
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+pub struct Person {
+    name: String,
+    age: u32,
+}
 
 async fn add2(Json(body): Json<u32>) -> Json<u32> {
     println!("{}", body + 2);
     Json(body + 2)
 }
 
+async fn person(Json(body): Json<Person>) -> String {
+    format!("{} is {} years old!", body.name, body.age)
+}
+
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new()
-        .route("/ping", get(|| async { "pong!\n" }))
-        .route("/add2", post(add2));
+        .route("/ping", get(|| async { "pong!" }))
+        .route("/add2", post(add2))
+        .route("/person", post(person));
 
     // run our app with hyper, listening globally on port 8080
     println!("Listening at port 8080...");
